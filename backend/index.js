6fde09239a435a583b1166832c05a38a784caf11
fathi <fathiclg@gmail.com>
2025-04-28 00:06:16 +0300

    server setUp Done

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import  express  from "express";
+import { PORT } from "./config.js";
+const app = express();
+
+
+app.get('/',(request, response)=>{
+console.log(request);
+return response.status(234).send('Welcome da')
+
+});
+app.listen(PORT,() => {
+    console.log(`App is listening to ${PORT}`);
+    
+});
